8fd51e7dec28b5f235b319a4b269ef24a7cce9bd
Hyun6Hub <sad765479@gmail.com>
2024-03-19 16:46:52 +0900

    portpolio_incomplete1

--- javascript/13.scope_json/4.var.js
+++ javascript/13.scope_json/4.var.js
@@ -1,18 +1,18 @@
 // var 키워드는 모두 전역으로 선언
 // 이름 중복 정의 가능
-var sname = 'hong';
-var sname = 'lee';
+var sname = "hong";
+var sname = "lee"; //재할당
 
-let kname = 'hong';
+let kname = "hong";
 // let kname = 'hong';
 
-  {
-    var sname = 'kim';
-  }
+{
+  var sname = "kim"; //최종적으론 kim출력
+}
 
-console.log(sname);
+console.log(sname); //kim출력
 
 function test() {
-  var a = 100;
+  var a = 100; //로컬 스코프안에서만 유효함
 }
 console.log(a);
